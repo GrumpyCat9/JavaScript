5c6718f6148fe7479fbc88c989c3ffd2f325a069
Nishad Sheriffdeen <nishadsheriffdeen@gmail.com>
2023-02-10 09:54:27 +0530

    random function practices

---
+++ Random JS/randomFunctions.js
@@ -0,0 +1,15 @@
+function triArea(base, height){
+    return (base * height) / 2
+}
+
+console.log(triArea(3,2))
+console.log(triArea(7,4))
+console.log(triArea(10,10))
+
+function howManySeconds(hours){
+    return (hours * 60) * 60
+}
+
+console.log(howManySeconds(2))
+console.log(howManySeconds(10))
+console.log(howManySeconds(24))
