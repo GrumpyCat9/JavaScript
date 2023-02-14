caf45c82005ec9e6dec364de519c1f005da24693
Nishad Sheriffdeen <nishadsheriffdeen@gmail.com>
2023-02-14 11:22:37 +0530

    Axios modified

--- API/Using axios/main.js
+++ API/Using axios/main.js
@@ -1,3 +1,5 @@
+let ID = document.getElementById("IDNo").innerText
+
 // GET REQUEST
 function getTodos() {
 /*   axios({
@@ -11,24 +13,52 @@ function getTodos() {
   .catch(error => console.log(error)) */
 
   axios
-  .get("https://my-burger-api.herokuapp.com/burgers")
+  .get('http://localhost:3000/arsenal/'+ ID)
   .then(result => showOutput(result))
   .catch(error => console.log(error)) 
+
+  console.log(ID)
 }
 
 // POST REQUEST
 function addTodo() {
-  console.log('POST Request');
+  axios.post("http://localhost:3000/arsenal",{
+    name: "classic",
+    type: "sidearm",
+    discription: "The pistol you always get for free at the beginning of each round, it’s serviceable with two firing modes: mouse-1 for single bullet (more accurate at a distance) or mouse-2 for a three bullet burst (better in close quarters).",
+    "primary fire": [
+      "semi-automatic",
+      "Fire rate: 6.75 rounds/sec"
+    ],
+    "Alt fire": [
+      "3 - round burst, spread increase",
+      "fire rate: 2.22 rounds/sec"
+    ],
+    Damage: [
+      "0-30m = Body 26, Head 78, Leg 22",
+      "30-50m = Body 22, Head 66, Leg 188"
+    ],
+    "Magazine Capacity": "12",
+    "Wall penetration": "low"})
+    
+  .then(result => showOutput(result))
+  .catch(error => console.log(error)) 
 }
 
 // PUT/PATCH REQUEST
 function updateTodo() {
-  console.log('PUT/PATCH Request');
+  axios
+    .put("http://localhost:3000/arsenal")
+    .then(result => showOutput(result))
+    .catch(error => console.log(error))
 }
 
 // DELETE REQUEST
 function removeTodo() {
-  console.log('DELETE Request');
+  axios
+    .delete("http://localhost:3000/arsenal/7")
+    .then(result => showOutput(result))
+    .catch(error => console.log(error))
 }
 
 // SIMULTANEOUS DATA
@@ -38,7 +68,35 @@ function getData() {
 
 // CUSTOM HEADERS
 function customHeaders() {
-  console.log('Custom Headers');
+
+  const config = {
+    header:{
+      'content-type': 'application/json',
+      Authorization: 'sometoken'
+    }
+  }
+
+  axios.post("http://localhost:3000/arsenal",{
+    name: "classic",
+    type: "sidearm",
+    discription: "The pistol you always get for free at the beginning of each round, it’s serviceable with two firing modes: mouse-1 for single bullet (more accurate at a distance) or mouse-2 for a three bullet burst (better in close quarters).",
+    "primary fire": [
+      "semi-automatic",
+      "Fire rate: 6.75 rounds/sec"
+    ],
+    "Alt fire": [
+      "3 - round burst, spread increase",
+      "fire rate: 2.22 rounds/sec"
+    ],
+    Damage: [
+      "0-30m = Body 26, Head 78, Leg 22",
+      "30-50m = Body 22, Head 66, Leg 188"
+    ],
+    "Magazine Capacity": "12",
+    "Wall penetration": "low"}, config)
+    
+  .then(result => showOutput(result))
+  .catch(error => console.log(error)) 
 }
 
 // TRANSFORMING REQUESTS & RESPONSES
@@ -57,6 +115,20 @@ function cancelToken() {
 }
 
 // INTERCEPTING REQUESTS & RESPONSES
+
+axios.interceptors.request.use(
+  config => {
+  console.log(
+    `${config.method.toUpperCase()} request sent to ${
+      config.url
+    } at ${new Date().getTime()}`
+    );
+
+  return config;},
+  error => {
+    return Promise.reject(error)
+  }
+  )
 
 // AXIOS INSTANCES
 
@@ -108,3 +180,5 @@ document
   .addEventListener('click', transformResponse);
 document.getElementById('error').addEventListener('click', errorHandling);
 document.getElementById('cancel').addEventListener('click', cancelToken);
+
+
